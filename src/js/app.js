28bcc1d00cbe711be0250213374787bb3a157fde
Niklas Smith <niklas_smith@hotmail.com>
2025-04-04 13:58:21 +0200

    make async function and get api with await fetch

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,27 @@
 
+let courses = [];
+
+window.onload = () => {
+    getCourses();
+
+    }
+
+
+    async function getCourses() {
+        try{
+        const resp = await fetch ("https://webbutveckling.miun.se/files/ramschema_ht24.json")
+        if(!resp.ok) {
+
+            throw new error("Kan inte läsa in")
+        }
+        courses = await resp.json();
+        loadCourses(courses)
+
+     
+        } catch(error) {
+
+            console.error(error);
+        }
+        
+    }
+
